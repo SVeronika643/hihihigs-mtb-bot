736b1d85e9bad9ee6ca176a9ba48b1c7200da1b4
SVeronika643 <snixon643@gmail.com>
2025-04-09 20:52:53 +0300

    изменила keyboard.py

--- handlers/keyboard.py
+++ handlers/keyboard.py
@@ -1,6 +1,3 @@
-import aiogram.types
-
-
 # TODO - создайте клавиатуру, которая будет появляться в сообщении либо находится там постоянно
 # Статичная клавиатура ReplyKeyboardMarkup https://docs.aiogram.dev/en/v3.15.0/api/types/reply_keyboard_markup.html
 # Динамически генерируемая клавиатура Keyboard builder https://docs.aiogram.dev/en/v3.15.0/utils/keyboard.html
@@ -8,11 +5,20 @@ import aiogram.types
 # Примеры создания клавиатуры Keyboard builder https://mastergroosha.github.io/aiogram-3-guide/buttons/
 
 
-# Здесь создать клавиатуры
-keyboard = None
-from aiogram.utils.keyboard import InlineKeyboardBuilder
 
-def get_random_keyboard():
-    builder = InlineKeyboardBuilder()
-    builder.button(text="Получить случайное значение", callback_data="random_value")
-    return builder.as_markup()
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+
+# Список кнопок для клавиатуры
+kb_list = [
+    [KeyboardButton(text="📖 О нас"),]
+]
+
+# Создание клавиатуры
+keyboard = ReplyKeyboardMarkup(keyboard=kb_list, resize_keyboard=True, one_time_keyboard=True)
+
+# Экспортируем переменную keyboard
+all = ['keyboard']
+
+
+# Импортируем клавиатуру
+#from handlers.keyboard import keyboard
