44c5c4bbbb59670a66cc7b134b6a0e2afe7bb229
Gashayn <iwantsomecheese01@mail.ru>
2025-05-02 05:32:00 +0300

    Update test_bot_main.py

--- tests/test_bot_main.py
+++ tests/test_bot_main.py
@@ -2,11 +2,11 @@ import pytest
 from main import main, set_commands
 from aiogram.types import BotCommand
 from unittest.mock import patch
-from fixtures import mock_bot, mock_router, mock_set_commands, mock_setuplogger, mock_dispatcher
+from fixtures import mock_bot, mock_router, mock_set_commands, mock_setuplogger, mock_dispatcher, mock_h_router, mock_c_router
 
 @pytest.mark.asyncio
-async def test_main_initialization(mock_bot, mock_router, mock_set_commands, mock_setuplogger, mock_dispatcher):
-    with patch(target="main.Bot", return_value=mock_bot), patch(target="main.Dispatcher", return_value=mock_dispatcher):
+async def test_main_initialization(mock_bot, mock_router, mock_set_commands, mock_setuplogger, mock_dispatcher, mock_h_router, mock_c_router):
+    with patch(target="main.Bot", return_value=mock_bot), patch(target="main.Dispatcher", return_value=mock_dispatcher), patch("handlers.handlers.router", mock_h_router), patch("handlers.callbacks.router", mock_c_router):
         await main()
         await set_commands(mock_bot)
         commands = [
@@ -17,5 +17,6 @@ async def test_main_initialization(mock_bot, mock_router, mock_set_commands, moc
         mock_setuplogger.assert_called_once()
         mock_dispatcher.startup.register.assert_called_once_with(mock_set_commands)
         mock_dispatcher.start_polling.assert_awaited_once_with(mock_bot)
-        mock_dispatcher.include_router.assert_called_once()
+        mock_dispatcher.include_router.assert_any_call(mock_h_router)
+        mock_dispatcher.include_router.assert_any_call(mock_c_router)
         mock_bot.set_my_commands.assert_awaited_once_with(commands)
