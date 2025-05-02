d96ad654d6253117e3e3e4ac11c06351cad121d3
Gashayn <iwantsomecheese01@mail.ru>
2025-05-02 05:31:19 +0300

    Update fixtures.py

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -30,3 +30,34 @@ def mock_dispatcher():
     mock.include_router = MagicMock()
 
     return mock
+
+@pytest.fixture
+def mock_message():
+    mock_msg = AsyncMock(spec=Message)
+    mock_msg.from_user = AsyncMock()
+    mock_msg.from_user.id = AsyncMock()
+    mock_msg.from_user.username = AsyncMock()
+    mock_msg.answer = AsyncMock()
+    mock_msg.chat = AsyncMock()
+    mock_msg.send_copy = AsyncMock()
+
+    return mock_msg
+
+@pytest.fixture
+def mock_callback_query():
+    mock = MagicMock(spec=CallbackQuery)
+    mock.data = "button_callback"
+    mock.message = MagicMock()
+    mock.message.answer = AsyncMock()
+    mock.from_user = MagicMock()
+    mock.from_user.username = "test_user"
+    
+    return mock
+
+@pytest.fixture
+def mock_c_router():
+    return MagicMock()
+
+@pytest.fixture
+def mock_h_router():
+    return MagicMock()
